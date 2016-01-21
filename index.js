678412bffa593f16c674a91b9b1430e8c732d511
Dave Yeu <daveyeu@gmail.com>
2016-01-21 11:20:46 -0500

    Change default port and add healthcheck endpoint

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app     = express()
-var port    = process.env.PORT || 3000
+var port    = process.env.PORT || 7001
 
 app.get('/', function(req, res) {
     res.send('Hello World!')
+})
+
+app.get('/healthcheck', function(req, res) {
+    res.status(200).end()
 })
 
 app.listen(port, function() {
